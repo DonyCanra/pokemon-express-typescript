7f502e2d7b8f8096f281d37361a3c851092b79b1
DonyCanra <donicanrarofikar@gmail.com>
2024-02-18 20:01:44 +0700

    Create: api fetchPokemonRandom & fetchPokemonByWord

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import axios, { AxiosRequestConfig } from 'axios';
 
 class Controller {
-  public static async fetchDatabase(req: Request, res: Response, next: NextFunction): Promise<void> {
+  public static async fetchPokemon(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
       const { limit = '12', page = '1' } = req.query as Record<string, string>;
 
@@ -29,6 +29,57 @@ class Controller {
       next(err);
     }
   }
+
+  public static async fetchPokemonRandom(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      const { limit = '12' } = req.query as Record<string, string>;
+
+
+      const { data } = await axios.get(`https://id.portal-pokemon.com/play/pokedex/api/v1/random?limit=${limit}`);  
+      
+      const updatedData = data.pokemons.map((item: any) => {
+        item.file_name = "https://id.portal-pokemon.com/play/resources/pokedex" + item.file_name;
+        return item;
+      });
+
+      res.status(200).json({
+        "status": true,
+        "error": null,
+        "data": updatedData,
+        "meta": {
+          "limit": limit,
+        },
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  public static async fetchPokemonByWord(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      const { word = 'Mega' } = req.query as Record<string, string>;
+
+
+      const { data } = await axios.get(`https://id.portal-pokemon.com/play/pokedex/api/v1?key_word=${word}`);  
+      
+      const updatedData = data.pokemons.map((item: any) => {
+        item.file_name = "https://id.portal-pokemon.com/play/resources/pokedex" + item.file_name;
+        return item;
+      });
+
+      res.status(200).json({
+        "status": true,
+        "error": null,
+        "data": updatedData,
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+
 }
 
 export default Controller;
+
+// https://id.portal-pokemon.com/play/pokedex/api/v1?key_word=bul
+// https://id.portal-pokemon.com/play/pokedex/api/v1/random?limit=13
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,8 @@ import Controller from '../controllers';
 const router = express.Router()
 
 // define the home page route
-router.get('/', Controller.fetchDatabase)
+router.get('/', Controller.fetchPokemon)
+router.get('/random', Controller.fetchPokemonRandom)
+router.get('/search', Controller.fetchPokemonByWord)
 
 module.exports = router
